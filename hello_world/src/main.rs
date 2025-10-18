9f9b224e5dc1e1a7bdfe611444aceebba42e020f
aniketh <techrunstheworld@gmail.com>
2025-10-18 22:36:23 +0530

    feat: counter-program

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut count = 0;
+
+    for _ in 0..10 {
+        count += 1;
+        println!("Count: {}", count);
+    }
+
+    println!("Final count: {}", count);
 }
