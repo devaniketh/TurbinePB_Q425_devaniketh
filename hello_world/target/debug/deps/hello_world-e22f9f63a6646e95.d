de49dcee190f5c0c35922b89c26123004c73b958
aniketh <techrunstheworld@gmail.com>
2025-10-18 22:33:25 +0530

    feat: hello-world cargo

---
+++ hello_world/target/debug/deps/hello_world-e22f9f63a6646e95.d
@@ -0,0 +1,9 @@
+/home/aniketh/projects/TurbinePB_Q425_devaniketh/hello_world/target/debug/deps/hello_world-e22f9f63a6646e95.d: src/main.rs Cargo.toml
+
+/home/aniketh/projects/TurbinePB_Q425_devaniketh/hello_world/target/debug/deps/libhello_world-e22f9f63a6646e95.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
